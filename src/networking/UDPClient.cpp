757e3dc477073f69defade5272d0f9d980cb0610
sharan669 <sharansurendra1995@gmail.com>
2020-11-29 21:32:44 -0500

    Make HandleIncomingPacket() extract CRDTAction from packet

--- src/networking/UDPClient.cpp
+++ src/networking/UDPClient.cpp
@@ -6,90 +6,82 @@
 #include "UDPClient.hpp"
 #include<iostream>
 
-
 UDPClient::UDPClient(unsigned short port, std::vector<struct PeerAddress> &peer_addresses) {
   this->client_port = port;
   this->peer_addresses = peer_addresses;
-  for(PeerAddress peer: peer_addresses){
-    std::cout<<peer.ip_address<<"\t"<<peer.port<<std::endl;
+  for (PeerAddress peer: peer_addresses) {
+    std::cout << peer.ip_address << "\t" << peer.port << std::endl;
   }
   Init();
 }
 
 void UDPClient::Init() {
   // Bind socket to port
- client_socket.bind(this->client_port);
- client_socket.setBlocking(false);
+  client_socket.bind(this->client_port);
+  client_socket.setBlocking(false);
   // Initialize socket
   std::thread ListeningThread(&UDPClient::StartListeningThread, this);
   //ListeningThread.detach(); - need to try this
   thread_vector.push_back(std::move(ListeningThread));
 }
 
-CRDTOperation operation;
-std::string site_id; // unique id of the client
-int counter; // site counter managed by version vector
-std::string text; // value of the string (current support for single chars) to be inserted.
 
 void UDPClient::StartListeningThread() {
-  std::cout<<"Listening "<<std::endl;
+  std::cout << "Listening " << std::endl;
   client_listening.store(true, std::memory_order_relaxed);
-  while(client_listening.load(std::memory_order_relaxed)){
+
+  while (client_listening.load(std::memory_order_relaxed)) {
     sf::Packet packet;
     sf::IpAddress sender;
     unsigned short port;
     sf::Socket::Status status;
-    status = client_socket.receive(packet, sender,port);
+    status = client_socket.receive(packet, sender, port);
 
-    if(status == sf::Socket::Status::Done)
-    {
-    int operation;
-    std::string site_id; // unique id of the client
-    int counter; // site counter managed by version vector
-    std::string text; // value of the string (current support for single chars) to be inserted.
-    int positions_size;
-    std::vector<int> positions; // fractional position calculated by CRDT.
-
-    packet >>operation >> site_id >> counter >> text >>positions_size;
-
-    for(int i=0;i<positions_size;i++){
-      int position;
-      packet>>position;
-      positions.push_back(position);
-    }
-    CRDTAction crdt_action((CRDTOperation) operation, site_id, counter, text, positions);
-    crdt_action.Print();
-    client_callbacks->OnRemoteOperationReceive(crdt_action);
+    if (status == sf::Socket::Status::Done) {
+      this->HandleIncomingPacket(packet);
     }
   }
-  std::cout<<"Stopped Listening Thread"<<std::endl;
+  std::cout << "Listening terminated" << std::endl;
 }
 
-
-
 void UDPClient::BroadcastActionToAllConnectedPeers(CRDTAction &crdt_action) {
-  for(PeerAddress peer_address: this->peer_addresses){
+  for (PeerAddress peer_address: this->peer_addresses) {
     sf::Packet packet;
     packet << crdt_action.Operation() << crdt_action.SiteId() << crdt_action.Counter() << crdt_action.Text() <<
-    (int)crdt_action.Positions().size();
+           (int) crdt_action.Positions().size();
 
-    for(int position:crdt_action.Positions()){
-      packet<<position;
+    for (int position:crdt_action.Positions()) {
+      packet << position;
     }
-    std::cout<<"Broadcast - "<<peer_address.ip_address<<"\t"<<"port"<<peer_address.port<<std::endl;
-    client_socket.send(packet,peer_address.ip_address,peer_address.port);
+    std::cout << "Broadcast - " << peer_address.ip_address << "\t" << "port" << peer_address.port << std::endl;
+    client_socket.send(packet, peer_address.ip_address, peer_address.port);
   }
-
 }
 
 void UDPClient::HandleIncomingPacket(sf::Packet &packet) {
-  //client_callbacks->OnRemoteOperationReceive(<packet parsed to get CRDTAction>);
+  int operation;
+  std::string site_id; // unique id of the client
+  int counter; // site counter managed by version vector
+  std::string text; // value of the string (current support for single chars) to be inserted.
+  int positions_size;
+  std::vector<int> positions; // fractional position calculated by CRDT.
+
+  packet >> operation >> site_id >> counter >> text >> positions_size;
+
+  for (int i = 0; i < positions_size; i++) {
+    int position;
+    packet >> position;
+    positions.push_back(position);
+  }
+  CRDTAction crdt_action((CRDTOperation) operation, site_id, counter, text, positions);
+  crdt_action.Print();
+  client_callbacks->OnRemoteOperationReceive(crdt_action);
 }
 
 void UDPClient::ShutdownClient() {
   client_listening.store(false, std::memory_order_relaxed);
-  for (auto& th : thread_vector) {
-   th.join();
+  for (auto &th : thread_vector) {
+    th.join();
   }
 }
 
